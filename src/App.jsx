d92a8d54b19cff1cbda3bf7abfee3540c960e95a
janidu <jchamoth12@gmail.com>
2024-10-14 10:31:18 -0700

    setup the project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const App = () =>(
+  
+    
+
+  <div className="bg-gradient-to-r from-secondary to-primary p-4 rounded-lg">
+  <h1 className="text-white">Hello World</h1>
+  <p className="text-yellow-500">This text is yellow.</p>
+
+</div>
+
+
+
+);
+
+  
+
+
+
+export default App
