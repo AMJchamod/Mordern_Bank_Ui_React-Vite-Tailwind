b5d4cefd203b511ad1ca572eaa5f98e0c4541377
AMJchamod <jchamoth12@gmail.com>
2024-10-31 12:11:16 +0530

    hero section done

--- src/components/Getstared.jsx
+++ src/components/Getstared.jsx
@@ -2,8 +2,8 @@ import { arrowUp } from '../assets';
 import styles from '../style';
 
 const Getstared = () => (
-    <div className={`${styles.flexcenter} w-[160px] p-1 h-[160px] bg-blue-gradient rounded-full cursor-pointer`}>
-        <div className={`${styles.flexCenter} rounded-full bg-primary w-full h-full flex-col`}>
+    <div className={`${styles.flexcenter} w-[160px] p-1 h-[160px] bg-blue-gradient rounded-full cursor-pointer mr-40`}>
+        <div className={`${styles.flexCenter} rounded-full bg-primary w-full h-full flex-col `}>
             <div className={`${styles.flexCenter} flex-row`}>
             <p className='font-poppins font-medium text-[18px] leading-[23px]'>
                     <span className='text-gradient'>Get </span>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
+import React from 'react';
 import styles from '../style';
 import { discount, robot } from '../assets';
 import Getstared from './Getstared';
 
-
-
-const Hero = () => (                                          //i just use  custom styles in here
-  <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY} `}>
+const Hero = () => (
+  <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
       <div className={`flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2`}>
-        <img src={discount} alt='discount' className='w-[32px] h-[32px]'></img>
+        <img src={discount} alt='discount' className='w-[32px] h-[32px]' />
         <p className={`${styles.paragraph} ml-2`}>
           <span className="text-white">20%</span>
           <span> Discount For{' '}</span>
-          <span className="text-white"> 1 Month</span>
+          <span className="text-white">1 Month</span>
           <span> Account</span>
         </p>
       </div>
       <div className="flex flex-row justify-between items-center w-full">
-        <h1 className='flex-1 font-poppins font-semibold ss:text-[72px]  text-[52px] text-white ss:leading-[100px] leading-[75px]'>The NEXT<br className='sm:block hidden' /> {""} <span className='text-gradient'>Generation</span><br /> Payment Method</h1>
-      </div>
-    </div>
-    <Getstared/>
-  </section>
-  
-    
-)
+        <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
+          The NEXT<br className='sm:block hidden' /> {""} <span className='text-gradient'>Generation</span><br /> Payment Method
+        </h1>
+        <div className=' ml-4 ss:flex hidden md:mr-4 '>
+          <Getstared />
+        </div>
 
-export default Hero
+      </div>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        our team of exprets uses a mehodology to identify  the credit cards most likely to fit your needs. we examine  annual percentage rates, annual fees.
+      </p>
+    
+    </div>
+    <div>
+    <img src={robot} alt='robot' className="w-[100%] h-[100%] relative z-[5]" />
+    </div>
+   
+  </section>
+);
+
+export default Hero;
