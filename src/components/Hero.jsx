ac1f173e7bf40c61f1d3ec01443cab7e16358556
AMJchamod <jchamoth12@gmail.com>
2024-11-04 14:54:26 +0530

    add color layers to the robort hand

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,10 +27,15 @@ const Hero = () => (
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         our team of exprets uses a mehodology to identify  the credit cards most likely to fit your needs. we examine  annual percentage rates, annual fees.
       </p>
-    
+
     </div>
-    <div>
+   
+    <div> 
     <img src={robot} alt='robot' className="w-[100%] h-[100%] relative z-[5]" />
+    <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
+    <div className='absolute z-[1] w-[80%] h-[80%] top-0 pink__gradient rounded-full bottom-40 white__gradient'></div>
+    <div className='absolute z-[0] w-[50%] h-[50%] top-0 pink__gradient right-20 bottom-20 blue__gradient'></div>
+     
     </div>
    
   </section>
