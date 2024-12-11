11ad8763d114d7e7936655a210b9926e06825c54
janidu <jchamoth12@gmail.com>
2024-12-11 04:58:44 -0800

    optimize the mobile view of  footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,10 +18,10 @@ const Footer = () =>(
           <h4>
             {footerlink.title}
           </h4>
-          <ul>
+          <ul className='list-none mt-4'>
              {footerlink.links.map((link,index)=>(
-              <li key={link.name} className='font-poppins font-normal text-[16px] leading-[24px] text-dimWhite 
-              hover:text-secondary cursor-pointer'>
+              <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite 
+              hover:text-secondary cursor-pointer${index !== footerlink.links.length - 1 ? 'mb-4' : 'mb-0'}`}>
                 {link.name}
               </li> 
              ))}
